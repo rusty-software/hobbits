37d014e3b2afaa2de2ba84bc95edf81089c4feec
Rusty Bentley <mycoding.alert@gmail.com>
2015-11-22 11:47:49 -0600

    uncommented test, fixed set expectation

--- test/hobbits/core_test.clj
+++ test/hobbits/core_test.clj
@@ -8,21 +8,21 @@
           expected [{:name "eye 1" :size 1}
                     {:name "eye 2" :size 1}
                     {:name "eye 3" :size 1}]]
-      (is (= #{expected} #{(symmetrize-body-part 3 part)})))))
+      (is (= (set expected) (set (symmetrize-body-part 3 part)))))))
 
-;(deftest general-symmetrize-body-parts-tests
-;  (testing "Given a map of body parts and needing 3 total, return a map with 1, 2, and 3"
-;    (let [parts [{:name "eye" :size 1}
-;                 {:name "ear" :size 1}
-;                 {:name "shoulder" :size 3}]
-;          expected [{:name "eye 1" :size 1}
-;                    {:name "eye 2" :size 1}
-;                    {:name "eye 3" :size 1}
-;                    {:name "ear 1" :size 1}
-;                    {:name "ear 2" :size 1}
-;                    {:name "ear 3" :size 1}
-;                    {:name "shoulder 1" :size 3}
-;                    {:name "shoulder 2" :size 3}
-;                    {:name "shoulder 3" :size 3}]
-;          actual (general-symmetrize-body-parts parts 3)]
-;      (is (= #{expected} #{actual})))) )
+(deftest general-symmetrize-body-parts-tests
+  (testing "Given a map of body parts and needing 3 total, return a map with 1, 2, and 3"
+    (let [parts [{:name "eye" :size 1}
+                 {:name "ear" :size 1}
+                 {:name "shoulder" :size 3}]
+          expected [{:name "eye 1" :size 1}
+                    {:name "eye 2" :size 1}
+                    {:name "eye 3" :size 1}
+                    {:name "ear 1" :size 1}
+                    {:name "ear 2" :size 1}
+                    {:name "ear 3" :size 1}
+                    {:name "shoulder 1" :size 3}
+                    {:name "shoulder 2" :size 3}
+                    {:name "shoulder 3" :size 3}]
+          actual (general-symmetrize-body-parts parts 3)]
+      (is (= (set expected) (set actual))))) )
